90c78a49905e6f2966aeaa4a5102c387983d163a
Kaito-Dogi <kendobu0405@gmail.com>
2026-05-17 00:17:40 +0900

    style: 画面全体を暗くした

--- app/src/main/kotlin/app/kaito_dogi/agsl_sample/App.kt
+++ app/src/main/kotlin/app/kaito_dogi/agsl_sample/App.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import app.kaito_dogi.agsl_sample.shader.Sandbox
 
 @Composable
@@ -14,6 +15,7 @@ internal fun App(
   modifier: Modifier = Modifier
 ) = Scaffold(
   modifier = modifier,
+  containerColor = Color.Black,
 ) { innerPadding ->
   Box(
     modifier = Modifier
