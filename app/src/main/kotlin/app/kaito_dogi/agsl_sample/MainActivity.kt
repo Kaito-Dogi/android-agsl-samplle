194f862a552a2c15378bb6c7774383b1100656a1
Kaito-Dogi <kendobu0405@gmail.com>
2026-05-16 23:50:52 +0900

    feat: first commit

---
+++ app/src/main/kotlin/app/kaito_dogi/agsl_sample/MainActivity.kt
@@ -0,0 +1,17 @@
+package app.kaito_dogi.agsl_sample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContent {
+      Text(text = "AGSL Sample")
+    }
+  }
+}
